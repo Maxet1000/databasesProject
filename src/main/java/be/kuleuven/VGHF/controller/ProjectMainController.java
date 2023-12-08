5ebccc4f3ae19977bc0340ede22f802c9baa9bb1
bjbjorn <bjorn.spauwen@student.uhasselt.be>
2023-12-08 13:31:57 +0100

    button layout op startscherm

--- src/main/java/be/kuleuven/VGHF/controller/ProjectMainController.java
+++ src/main/java/be/kuleuven/VGHF/controller/ProjectMainController.java
@@ -3,26 +3,36 @@ package be.kuleuven.VGHF.controller;
 import be.kuleuven.VGHF.ProjectMain;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class ProjectMainController {
 
     @FXML
-    private Button btnComputerConfigs;
+    private Button btn1;
     @FXML
-    private Button btnHwComponenten;
+    private Button btn2;
     @FXML
-    private Button btnConfigAttaches;
+    private Button btn3;
+    @FXML
+    private Text txt1;
+    @FXML
+    private StackPane pane;
 
     public void initialize() {
-        btnHwComponenten.setOnAction(e -> showBeheerScherm("hwcomponenten"));
-        btnComputerConfigs.setOnAction(e -> showBeheerScherm("computerconfigs"));
-        btnConfigAttaches.setOnAction(e -> showBeheerScherm("attaches"));
+        btn1.setOnAction(e -> showHomeScherm("hwcomponenten"));
+        btn2.setOnAction(e -> showBeheerScherm("computerconfigs"));
+        btn3.setOnAction(e -> showBeheerScherm("attaches"));
+        pane.setAlignment(txt1, Pos.CENTER);
     }
+
 
     private void showBeheerScherm(String id) {
         var resourceName = "beheer" + id + ".fxml";
@@ -39,5 +49,9 @@ public class ProjectMainController {
         } catch (Exception e) {
             throw new RuntimeException("Kan beheerscherm " + resourceName + " niet vinden", e);
         }
+    }
+
+    private void showHomeScherm(String hwcomponenten) {
+        txt1.setRotate(txt1.getRotate()+30);
     }
 }
