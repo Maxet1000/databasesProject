a1cae485959db4b3b44f4eb649902f2695ba1391
Dries <Dries.Ruttens@student.uhasselt.be>
2023-12-13 18:47:55 +0100

    only 1 copy in cart

--- src/main/java/be/kuleuven/VGHF/controller/RentgamesController.java
+++ src/main/java/be/kuleuven/VGHF/controller/RentgamesController.java
@@ -5,22 +5,20 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
+import org.hibernate.internal.util.MarkerObject;
+
 import be.kuleuven.VGHF.ProjectMain;
 import be.kuleuven.VGHF.domain.Console;
 import be.kuleuven.VGHF.domain.Developer;
 import be.kuleuven.VGHF.domain.Genre;
 import be.kuleuven.VGHF.enums.Availability;
 import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.CheckBoxTreeCell;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import java.util.List;
 
@@ -90,12 +88,15 @@ public class RentgamesController {
 
         System.out.println(selectedItem);
          
-        for (int i = 0; i < 1; i++){
             int x = (int) selectedItem.get(selectedItem.size()-1);
             var copy = ProjectMain.getDatabase().getCopyById(x);
             var gameName = copy.getGame().getTitle();
             var copyId = copy.getCopyID();
-            
+
+            boolean doubleCopy = false;
+    
+
+
                 String developers = "";
                 for (int j = 0; j < copy.getGame().getDevelopers().size(); j++) {
                     developers = developers+ copy.getGame().getDevelopers().get(j).getDeveloperName();
@@ -117,8 +118,22 @@ public class RentgamesController {
                         genres = genres + ", ";
                     }
                 }
-
+            
+                ObservableList<List> items = tblCart.getItems();
+                for (List item : items) {
+                    if (item.get(item.size()-1).equals(copyId)) {
+                        Alert alert = new Alert(Alert.AlertType.WARNING);
+                        alert.setTitle("Warning Dialog");
+                        alert.setHeaderText(null);
+                        alert.setContentText("The value already exists in the TableView.");
+                        alert.showAndWait();
+                        doubleCopy = true; 
+                    }
+                }
+        if (doubleCopy == false){
             tblCart.getItems().add(FXCollections.observableArrayList(gameName, developers, consoles, genres, copyId));
+        }else {
+            doubleCopy = false;
         }
     }
 
